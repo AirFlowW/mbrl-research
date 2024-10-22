eb908dd005a4353d4eae8d1bbfd21e7067325dad
AirFlowW <>
2024-10-22 15:19:55 +0200

    adapts pets to work with VBLLMLP ensemble

--- mbrl/algorithms/pets.py
+++ mbrl/algorithms/pets.py
@@ -13,6 +13,7 @@ import torch
 import mbrl.constants
 import mbrl.models
 from mbrl.models.vbll_mlp import VBLLMLP
+from mbrl.models.basic_ensemble import BasicEnsemble
 import mbrl.planning
 import mbrl.types
 import mbrl.util
@@ -111,8 +112,9 @@ def train(
         while not terminated and not truncated:
             # --------------- Model Training -----------------
             if env_steps % cfg.algorithm.freq_train_model == 0:
-                if isinstance(dynamics_model, VBLLMLP):
-                    dynamics_model.update_regularization_weight_from_dataset_length(replay_buffer.num_stored)
+                if isinstance(dynamics_model, BasicEnsemble) and isinstance(dynamics_model.members[0], VBLLMLP):
+                    for member in dynamics_model.members:
+                        member.update_regularization_weight_from_dataset_length(replay_buffer.num_stored)
                 mbrl.util.common.train_model_and_save_model_and_data(
                     dynamics_model,
                     model_trainer,
