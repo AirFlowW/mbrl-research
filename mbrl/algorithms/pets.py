ae3f890d14c08ef1ecd3a9350d776ca527c0037b
AirFlowW <>
2024-10-25 16:04:26 +0200

    adapts regularization weight update to different val_ratios

--- mbrl/algorithms/pets.py
+++ mbrl/algorithms/pets.py
@@ -113,8 +113,10 @@ def train(
             # --------------- Model Training -----------------
             if env_steps % cfg.algorithm.freq_train_model == 0:
                 if isinstance(dynamics_model, BasicEnsemble) and isinstance(dynamics_model.members[0], VBLLMLP):
+                    val_size = int(replay_buffer.num_stored * cfg.validation_ratio)
+                    train_size = replay_buffer.num_stored - val_size
                     for member in dynamics_model.members:
-                        member.update_regularization_weight_from_dataset_length(replay_buffer.num_stored)
+                        member.update_regularization_weight_from_dataset_length(train_size)
                 mbrl.util.common.train_model_and_save_model_and_data(
                     dynamics_model,
                     model_trainer,
